6f3c2eca6a7f6cd8e3d54d008804d9ae08a675b2
zhengcharlie8 <44006237+zhengcharlie8@users.noreply.github.com>
2020-10-25 05:53:20 -0400

    Initial commit

---
+++ threeinarow/src/model/RowGameModel.java
@@ -0,0 +1,44 @@
+package model;
+
+
+public class RowGameModel 
+{
+    public static final String GAME_END_NOWINNER = "Game ends in a draw";
+
+    public RowBlockModel[][] blocksData = new RowBlockModel[3][3];
+
+    /**
+     * The current player taking their turn
+     */
+    private String player = "1";
+    private int movesLeft = 9;
+    
+    
+    public String getPlayerTurn(){
+    	return player;
+    }
+    public int getMovesLeft() {
+    	return movesLeft;
+    }
+
+    private String finalResult = null;
+
+
+    public RowGameModel() {
+	super();
+
+	for (int row = 0; row < 3; row++) {
+	    for (int col = 0; col < 3; col++) {
+		blocksData[row][col] = new RowBlockModel(this);
+	    } // end for col
+	} // end for row
+    }
+
+    public String getFinalResult() {
+	return this.finalResult;
+    }
+
+    public void setFinalResult(String finalResult) {
+	this.finalResult = finalResult;
+    }
+}
